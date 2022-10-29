f69c98aa5378711c7ef759c1eebbd4c785147999
Orie McIntosh <dev@eighttwelveconsulting.com>
2022-10-29 15:31:01 -0400

    updated environment resource provisioning scripts

--- src/bash_scripts/aws-key-pair-creation.sh
+++ src/bash_scripts/aws-key-pair-creation.sh
@@ -2,9 +2,7 @@
 
 aws --profile acg-lab ec2 create-key-pair \
 	--key-name ansible-course-key-pair \
-	--key-type rsa \
-	--key-format pem \
-	--query "KeyMaterial" \
-	--output text > src/output/ansible-course-key-pair.pem
+	--output text > ansible-course-key-pair.pem
 
-sudo chmod 400 src/output/ansible-course-key-pair.pem
+mv ansible-course-key-pair.pem ../output/
+sudo chmod 400 ../output/ansible-course-key-pair.pem
