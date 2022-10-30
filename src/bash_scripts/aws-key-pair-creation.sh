d4d1c3bfbe0804d0bd80f697b4a947bf62ad322c
Orie McIntosh <dev@eighttwelveconsulting.com>
2022-10-30 11:42:47 -0400

    updated key pair creation script to use jq

--- src/bash_scripts/aws-key-pair-creation.sh
+++ src/bash_scripts/aws-key-pair-creation.sh
@@ -2,7 +2,7 @@
 
 aws --profile acg-lab ec2 create-key-pair \
 	--key-name ansible-course-key-pair \
-	--output text > ansible-course-key-pair.pem
+	| jq -r .KeyMaterial > ansible-course-key-pair.pem
 
 mv -f ansible-course-key-pair.pem ../output/
 sudo chmod 400 ../output/ansible-course-key-pair.pem
