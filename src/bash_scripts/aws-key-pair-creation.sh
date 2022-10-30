3c6021e95a373299b632fb313ec7801c6765d38b
Orie McIntosh <dev@eighttwelveconsulting.com>
2022-10-30 09:49:34 -0400

    updated scripts for ec2 key pair creation and creation of CloudFormation stack

--- src/bash_scripts/aws-key-pair-creation.sh
+++ src/bash_scripts/aws-key-pair-creation.sh
@@ -4,5 +4,5 @@ aws --profile acg-lab ec2 create-key-pair \
 	--key-name ansible-course-key-pair \
 	--output text > ansible-course-key-pair.pem
 
-mv ansible-course-key-pair.pem ../output/
+mv -f ansible-course-key-pair.pem ../output/
 sudo chmod 400 ../output/ansible-course-key-pair.pem
